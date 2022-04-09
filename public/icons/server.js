9ba0e148fb524eccc4fab28b51096dbb228f35d8
I am Billie <dosbor3@gmail.com>
2022-04-09 16:40:06 -0500

    updating mongoose connection

--- public/icons/server.js
+++ public/icons/server.js
@@ -6,11 +6,6 @@ const compression = require("compression");
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
 const app = express();
 
 app.use(logger("dev"));
@@ -20,6 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
