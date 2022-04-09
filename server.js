2847fc34bf69d79c651ee8a9e43f2a446f40a011
I am Billie <dosbor3@gmail.com>
2022-04-09 16:21:12 -0500

    code optimization

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-//const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
@@ -20,11 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
 
 // routes
 app.use(require("./routes/api.js"));
