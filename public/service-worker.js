f63d7af7a8d47f44828c3f8303454ea7dc18f26c
I am Billie <dosbor3@gmail.com>
2022-04-09 13:53:44 -0500

    cache implemented and tested

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,50 @@
-const APP_PREFIX = 'AABudgetTracker';
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./assets/js/idb.js",
+  "../dist/main.bundle.js"
+];
+
+const APP_PREFIX = 'AABudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CACHE = [
-    "./index.html",
-    "./assets/css/style.css",
-    "./dist/app.bundle.js",
-    "./dist/events.bundle.js",
-    "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
-  ];
-
   
 self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  )
+})
 
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+       return request || fetch(e.request)
+    })
+  )
 })
